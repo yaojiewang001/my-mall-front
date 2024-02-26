d8130da86d5ec2cf995b7c850176dd4d58181616
yaojiewang <935144133@qq.com>
2024-02-26 16:22:40 +0800

    测试-3删除HTTP

--- src/main-prod.js
+++ src/main-prod.js
@@ -26,7 +26,7 @@ import 'nprogress/nprogress.css'
 
 /* 配置请求根路径 */
 //axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
-axios.defaults.baseURL = "http://8.210.232.1:8800/api/private/v1/"
+axios.defaults.baseURL = "//8.210.232.1:8800/api/private/v1/"
 
 /* 请求拦截 */
 axios.interceptors.request.use(config=>{
